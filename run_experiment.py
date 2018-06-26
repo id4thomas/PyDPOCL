541734c8b9148bd9f562098daf79418a987123c6
id4thomas <id2thomas@gmail.com>
2018-06-27 01:14:56 +0900

    Fix import errors on macos(mojave beta)

--- run_experiment.py
+++ run_experiment.py
@@ -24,10 +24,10 @@ if __name__ == '__main__':
 	# 8] 4 agents, 1 car, 1 airplane, 4 locations, 2 goals
 	problem_file_8 = 'Ground_Compiler_Library//domains/travel-8.pddl'
 
-	problems = [problem_file_1, problem_file_2, problem_file_3, problem_file_4,
-	           problem_file_5, problem_file_6, problem_file_7, problem_file_8]
+	#problems = [problem_file_1, problem_file_2, problem_file_3, problem_file_4,
+	 #          problem_file_5, problem_file_6, problem_file_7, problem_file_8]
 	# problems = [problem_file_8]
-
+	problems = [problem_file_1, problem_file_2, problem_file_3]
 	d_name = domain_file.split('/')[-1].split('.')[0]
 
 	# for each problem, solve in 1 of 4 ways... but need way to run in different ways
@@ -36,7 +36,7 @@ if __name__ == '__main__':
 		p_name = prob.split('/')[-1].split('.')[0]
 		uploadable_ground_step_library_name = 'Ground_Compiler_Library//' + d_name + '.' + p_name
 
-		RELOAD = 0
+		RELOAD = 1
 		if RELOAD:
 			print('reloading')
 			ground_steps = just_compile(domain_file, prob, uploadable_ground_step_library_name)
