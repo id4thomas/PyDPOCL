8fd92005b7313daa533abe44710c4377aaff54d6
Youngrok Song <id2thomas@gmail.com>
2019-08-23 19:48:54 +0900

    Applied Diaper Story problem, added log files, and passed characters to GPlanner

--- run_experiment.py
+++ run_experiment.py
@@ -4,11 +4,11 @@ from PyDPOCL import *
 if __name__ == '__main__':
 
 	# domain_file = 'Ground_Compiler_Library//domains/travel_domain_primitive_only.pddl'
-	domain_file = 'Ground_Compiler_Library//domains/travel_domain.pddl'
+	domain_file = 'Ground_Compiler_Library//domains/diaper_domain.pddl'
 
 	# Problem files
 	# 1] 1 agent, 1 car, 1 airplane, 2 locations
-	problem_file_1 = 'Ground_Compiler_Library//domains/travel-to-la.pddl'
+	problem_file_1 = 'Ground_Compiler_Library//domains/diaper_story.pddl'
 	# 2] 2 agents, 1 car, 1 airplane, 2 locations, 1 goal
 	problem_file_2 = 'Ground_Compiler_Library//domains/travel-2.pddl'
 	# 3] 2 agents, 1 car, 1 airplane, 2 locations, 2 goals
@@ -24,9 +24,9 @@ if __name__ == '__main__':
 	# 8] 4 agents, 1 car, 1 airplane, 4 locations, 2 goals
 	problem_file_8 = 'Ground_Compiler_Library//domains/travel-8.pddl'
 
-	problems = [problem_file_1, problem_file_2, problem_file_3, problem_file_4,
-	           problem_file_5, problem_file_6, problem_file_7, problem_file_8]
-	# problems = [problem_file_8]
+	#problems = [problem_file_1, problem_file_2, problem_file_3, problem_file_4,
+	           #problem_file_5, problem_file_6, problem_file_7, problem_file_8]
+	problems = [problem_file_1]
 
 	d_name = domain_file.split('/')[-1].split('.')[0]
 
@@ -54,7 +54,34 @@ if __name__ == '__main__':
 		print('finished uploading')
 
 		print(p_name)
-
-		planner = GPlanner(ground_steps)
-		planner.solve(k=40, cutoff=1200)
+		characters=['a','b']
+		planner = GPlanner(ground_steps,characters)
+		out=planner.solve(k=3, cutoff=2500)
 		# planner.solve(k=40, cutoff=10)
+		plan_num=0
+		for plan in out:
+			#log files
+			out_f=open("./out_plan/plan_"+str(plan_num)+".txt",'w')
+			out_causal=open("./out_plan/causal_"+str(plan_num)+".txt",'w')
+			out_ordering=open("./out_plan/ordering_"+str(plan_num)+".txt",'w')
+
+			#write causal link to file
+			for lnk in list(plan.CausalLinkGraph.edges):
+				out_causal.write(str(lnk)+'\n')
+
+			#write ordering link to file
+			for lnk in list(plan.OrderingGraph.edges):
+				out_ordering.write(str(lnk)+'\n')
+
+			#write plan to file
+			for step in plan.OrderingGraph.topoSort():
+				print("%d step processing"%(i))
+				out_f.write("%d step processing"%(i))
+				print('\t\t{}\n'.format(str(step)))
+				out_f.write('\t\t{}\n'.format(str(step)))
+				i=i+1
+			#close log files
+			out_f.close()
+			out_ordering.close()
+			out_causal.close()
+			plan_num+=1
